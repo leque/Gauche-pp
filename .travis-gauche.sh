f1cd7e179e6473d2ab46addec42553623b628c9e
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2018-07-05 01:54:44 +0900

    use get-gauche.sh in .travis.yml

--- .travis-gauche.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-name="Gauche-${GAUCHE_VERSION}"
-source="$name.tgz"
-wget -O "$source" "http://prdownloads.sourceforge.net/gauche/$source"
-tar xzvf "$source"
-(cd $name; ./configure --prefix=$HOME; make; make install)
